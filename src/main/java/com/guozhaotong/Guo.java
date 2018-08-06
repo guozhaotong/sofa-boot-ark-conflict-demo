1275e8e103107c1e85cd86a5567d82f3d0ffd2d6
郭朝彤 <guochaotong@corp.netease.com>
2018-08-06 21:10:02 +0800

    初始尝试

---
+++ src/main/java/com/guozhaotong/Guo.java
@@ -0,0 +1,14 @@
+package com.guozhaotong;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guo {
+    public String outputList(){
+        List<String> list = new ArrayList<String>();
+        list.add("guo");
+        list.add("zhao");
+        list.add("tong");
+        return list.toString();
+    }
+}
