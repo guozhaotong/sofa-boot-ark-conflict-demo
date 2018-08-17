6f8407e08b54d62f066af9b36b42022594af372c
郭朝彤 <guochaotong@corp.netease.com>
2018-08-17 16:19:36 +0800

    "初始导入"

---
+++ src/main/java/com/guozhaotong/arkplugin/ArkClass.java
@@ -0,0 +1,9 @@
+package com.guozhaotong.arkplugin;
+
+import com.guozhaotong.myself.Person;
+
+public class ArkClass {
+    public String output(Person person){
+        return person.toString();
+    }
+}
